63fb03581282f7a134312cc540d4c0602a63350b
Courage Code <courageobunike@gmail.com>
2025-11-24 23:49:24 +0100

    made some chnages

---
+++ src/features/project/agile/modal/review.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Review = () => {
+  return (
+   <div>
+       <div>
+          <img src="/icon/checkImage.svg" alt="Check Mark" />
+          <h3>Review Sprint Plan</h3>
+       </div>
+   </div>
+  )
+}
+
+export default Review
